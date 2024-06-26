c1739821f256f654cac6ce224cdc8223ae66fd83
Mpho Nkala <mphon@synthesis.co.za>
2024-06-26 21:39:54 +0200

    styling cleanup

--- src/screens/LoginScreen.vue
+++ src/screens/LoginScreen.vue
@@ -8,16 +8,32 @@
         type="email"
         hint="Email Adress"
         error-message="must be valid email"
+        input-class="login-input-box-override"
         v-model:model-value="username"
-      />
+      >
+        <template v-slot:append>
+          <q-icon name="email" size="1.23rem" color="blue" /> </template
+      ></q-input>
+
       <q-input
         class="login-input-box"
         outlined
         type="password"
         hint="Password"
         v-model:model-value="password"
+        input-class="login-input-box-override"
+      >
+        <template v-slot:append>
+          <q-icon name="key" size="1.23rem" color="blue" />
+        </template>
+      </q-input>
+
+      <q-btn
+        unelevated
+        class="login-button"
+        label="login"
+        @click="submitLogin"
       />
-      <q-btn class="login-button" label="login" @click="submitLogin" />
     </div>
   </div>
 </template>
@@ -38,16 +54,12 @@ const submitLogin = async () => {
 
   await userAuthStore.logUserIn(loginData).then((res) =>{ applicationUserStore.setActivelApplicationUser(applicationUserStore)});
 
-  //console.log(applicationUserStore._activeApplicationUser)
-
-  //useApplicationUserStore().setActivelApplicationUser(response)
-  //applicationUserStore.setActivelApplicationUser(appUser)
 };
 </script>
 
 <style scoped>
 .login-screen-container {
-  background-color: rgb(162, 167, 168);
+  background-color: rgb(255, 255, 255);
   height: 100vh;
   padding-left: 30vw;
   padding-top: 10vh;
@@ -56,7 +68,7 @@ const submitLogin = async () => {
 .login-screen-banner {
   height: 70vh;
   width: 35vw;
-  background-color: rgb(149, 210, 239);
+  background-color: #95d2ef;
   border-radius: 20px;
   padding: 10px;
   padding-top: 70px;
@@ -75,10 +87,12 @@ const submitLogin = async () => {
   width: 80%;
   margin-left: 50px;
   margin-top: 100px;
-  background-color: rgb(200, 239, 242);
+  background-color: rgb(255, 255, 255);
   color: rgb(24, 49, 68);
   height: 50px;
   border-radius: 10px;
+  font-weight: bold;
+  font-size: 22;
 }
 
 .login-header-text {
@@ -88,5 +102,30 @@ const submitLogin = async () => {
   font-weight: bold;
   margin-bottom: 20px;
   color: rgb(24, 49, 68);
+}
+
+.login-input-box-override {
+}
+</style>
+
+<style>
+/* For Chrome */
+input:-webkit-autofill,
+textarea:-webkit-autofill,
+select:-webkit-autofill {
+  background-color: #95d2ef !important; /* Your custom background color */
+  -webkit-box-shadow: 0 0 0px 1000px #95d2ef inset !important; /* Same as background color */
+  -webkit-text-fill-color: #000000 !important; /* Your text color */
+  transition: background-color 5000s ease-in-out 0s;
+}
+
+/* For Firefox */
+input:-moz-autofill,
+textarea:-moz-autofill,
+select:-moz-autofill {
+  background-color: #95d2ef !important; /* Your custom background color */
+  -moz-box-shadow: 0 0 0px 1000px #95d2ef inset !important; /* Same as background color */
+  -moz-text-fill-color: #000000 !important; /* Your text color */
+  transition: background-color 5000s ease-in-out 0s;
 }
 </style>
