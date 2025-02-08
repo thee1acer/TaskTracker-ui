42b2dda2516ad58872dd2747fe2dbb011261eef0
Mpho Nkala <mphonkala.info@gmail.com>
2025-02-08 23:16:36 +0200

    revamp login screen

--- src/screens/LoginScreen.vue
+++ src/screens/LoginScreen.vue
@@ -1,39 +1,65 @@
 <template>
-  <div class="login-screen-container">
-    <div class="login-screen-banner">
-      <div class="login-header-text">TaskTracker</div>
-      <q-input
-        class="login-input-box"
-        outlined
-        type="email"
-        hint="Email Adress"
-        error-message="must be valid email"
-        input-class="login-input-box-override"
-        v-model:model-value="username"
-      >
-        <template v-slot:append>
-          <q-icon name="email" size="1.23rem" color="blue" /> </template
-      ></q-input>
+  <div class="flex w-full p-6">
+    <div
+      id="companyBannerSide"
+      class="h-screen w-1/2 bg-teal-200 rounded-lg flex justify-center items-center"
+    >
+      Welcome!
+    </div>
 
-      <q-input
-        class="login-input-box"
-        outlined
-        type="password"
-        hint="Password"
-        v-model:model-value="password"
-        input-class="login-input-box-override"
-      >
-        <template v-slot:append>
-          <q-icon name="key" size="1.23rem" color="blue" />
-        </template>
-      </q-input>
+    <div
+      id="loginOrRegisterSide"
+      class="flex h-lvh w-1/2 justify-center items-center"
+    >
+      <div class="grid w-full justify-center p-4 gap-y-2">
+        <div id="companyLogoContainer" class="flex justify-center items-center">
+          <img
+            src="/src/assets/logo/tasktrackerlogo.svg"
+            class="w-16 h-20 object-cover"
+          />
+        </div>
 
-      <q-btn
-        unelevated
-        class="login-button"
-        label="login"
-        @click="submitLogin"
-      />
+        <span class="text-2xl text-center">TaskTracker</span>
+
+        <div class="p-4 flex flex-col space-y-3">
+          <q-input
+            outlined
+            label="email adress"
+            stack-label
+            type="email"
+            dense
+            error-message="Must be a valid email"
+            input-class="login-input-box-override"
+            v-model="username"
+          >
+            <template v-slot:append>
+              <q-icon name="email" size="1.23rem" color="blue" />
+            </template>
+          </q-input>
+
+          <q-input
+            outlined
+            label="password"
+            stack-label
+            dense
+            type="password"
+            v-model="password"
+            input-class="login-input-box-override"
+          >
+            <template v-slot:append>
+              <q-icon name="key" size="1.23rem" color="blue" />
+            </template>
+          </q-input>
+
+          <q-btn
+            color="teal-6"
+            label="Login"
+            no-caps
+            @click="submitLogin"
+            class="w-full"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -57,83 +83,3 @@ const submitLogin = async () => {
   if(response != undefined) applicationUserStore.setActivelApplicationUser(response);
 };
 </script>
-
-<style scoped>
-.login-screen-container {
-  background-color: rgb(255, 255, 255);
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.login-screen-banner {
-  background-color: rgb(244, 238, 238);
-  border-radius: 20px;
-  padding: 10px;
-  padding-top: 70px;
-  border: 1px solid rgb(200, 197, 197);
-  box-shadow: 1px;
-  width: 500px;
-}
-
-.login-input-box {
-  margin-bottom: 15px;
-  display: grid;
-  margin-left: 15%;
-  width: 70%;
-  align-self: center;
-}
-
-.login-button {
-  width: 80%;
-  margin-left: 10%;
-  margin-top: 20%;
-  background-color: rgb(255, 255, 255);
-  color: rgb(24, 49, 68);
-  height: 50px;
-  border-radius: 10px;
-  font-weight: bold;
-  font-size: 22;
-}
-
-.login-header-text {
-  width: 100%;
-  text-align: center;
-  font-size: x-large;
-  font-weight: bold;
-  margin-bottom: 20px;
-  color: rgb(24, 49, 68);
-}
-
-.login-input-box-override {
-}
-</style>
-
-<style>
-body {
-  transform: scale(1);
-  transform-origin: 0 0;
-}
-
-/* For Chrome */
-input:-webkit-autofill,
-textarea:-webkit-autofill,
-select:-webkit-autofill {
-  background-color: #f4eeee !important;
-  -webkit-box-shadow: 0 0 0px 1000px #f4eeee inset !important;
-  -webkit-text-fill-color: #000000 !important;
-  transition: background-color 5000s ease-in-out 0s;
-}
-
-/* For Firefox */
-input:-moz-autofill,
-textarea:-moz-autofill,
-select:-moz-autofill {
-  background-color: #f4eeee !important;
-  -moz-box-shadow: 0 0 0px 1000px #f4eeee inset !important;
-  -moz-text-fill-color: #000000 !important;
-  transition: background-color 5000s ease-in-out 0s;
-}
-</style>
